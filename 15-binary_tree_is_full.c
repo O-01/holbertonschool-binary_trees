3f943f9f2de614e4619a5364407ea8f73b70a85b
O-01 <sa.an-s@yandex.com>
2023-08-16 17:37:20 -0500

    Betty formatting - is_full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * binary_tree_is_full - verifies if tree has only 0 or 2 nodes
+ * @tree: object tree
+ * Return: 1 upon full, 0 upon NULL tree or if tree has 1 node
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
