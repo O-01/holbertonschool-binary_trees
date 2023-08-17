5f12d22443b0902bf226a9726fd962b5cf498761
O-01 <sa.an-s@yandex.com>
2023-08-16 21:50:33 -0500

    Modified full, added sibling & uncle

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,18 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - verifies if tree has only 0 or 2 nodes
+ * binary_tree_is_full - verifies if binary tree is full
  * @tree: object tree
  * Return: 1 upon full, 0 upon NULL tree or if tree has 1 node
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	int full = 0;
+
+	if (!tree ||
+	    (!tree->left && tree->right) ||
+	    (tree->left && !tree->right))
 		return (0);
 
-	if (tree->parent && tree->left && tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
 
-	return (0);
+	full = binary_tree_is_full(tree->left) *
+		binary_tree_is_full(tree->right);
+
+	return (full);
 }
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds sibling of node
+ * @node: object node
+ * Return: pointer to sibling node, NULL upon not found or NULL input node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
